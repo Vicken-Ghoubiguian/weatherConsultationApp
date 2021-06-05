19679ae33c1f88c415db44b496ff33b2d7be82cc
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-06-05 10:52:39 +0200

    Writting some comments

--- app/src/main/java/com/example/weatherconsultationapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherconsultationapp/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.weatherconsultationapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-//
+// Definition of the 'MainActivity' class which define the main activity controller...
 class MainActivity : AppCompatActivity() {
 
-    //
+    // Overriden function to create the main activity...
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        //
+        // Definition of the main activity...
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
