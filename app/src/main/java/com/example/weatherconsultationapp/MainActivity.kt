5b1a7a9366d1af22e800422ea4dbb1925b2bcedf
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-06-05 11:16:02 +0200

    Give me something...Sequel...

--- app/src/main/java/com/example/weatherconsultationapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherconsultationapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.weatherconsultationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 // Definition of the 'MainActivity' class which define the main activity controller...
 class MainActivity : AppCompatActivity() {
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //
-        
+        // Extraction of informed openWeather API Key value...
+        val openWeatherAPIKey: String = (findViewById(R.id.openWeatherAPIKey) as TextView).text.toString()
     }
 }
