5b3d9fcaa11088de232deb1f1ba433979a20728f
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-02-19 11:43:55 +0100

    GL

--- app/src/main/java/com/example/weatherconsultationapp/openweathermap/GeographicLocation.kt
+++ app/src/main/java/com/example/weatherconsultationapp/openweathermap/GeographicLocation.kt
@@ -2,4 +2,28 @@ package com.example.weatherconsultationapp.openweathermap
 
 //
 class GeographicLocation {
+
+    //
+    private var countryCode: String? = null
+    private var cityName: String? = null
+
+    //
+    constructor(countryCode: String, cityName: String) {
+
+        //
+        this.countryCode = countryCode
+        this.cityName = cityName
+    }
+
+    //
+    public fun getCountryCode(): String? {
+
+        return this.countryCode
+    }
+
+    //
+    public fun getCityName(): String? {
+
+        return this.cityName
+    }
 }
