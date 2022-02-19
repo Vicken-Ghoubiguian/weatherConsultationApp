539fcd08ffd22d5bbb2cbbfdfdf98b46dfbe6345
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-02-19 11:00:10 +0100

    Adding some classes

---
+++ app/src/main/java/com/example/weatherconsultationapp/openweathermap/TemperatureUnitEnum.kt
@@ -0,0 +1,4 @@
+package com.example.weatherconsultationapp.openweathermap
+
+enum class TemperatureUnitEnum {
+}
---
+++ app/src/main/java/com/example/weatherconsultationapp/openweathermap/UV.kt
@@ -0,0 +1,4 @@
+package com.example.weatherconsultationapp.openweathermap
+
+class UV {
+}
