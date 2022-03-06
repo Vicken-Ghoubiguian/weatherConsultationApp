e7908edd12176c1d585c2d9fc32df0f27e706792
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-03-06 21:28:42 +0100

    JKjJKJK

--- app/src/main/java/com/example/weatherconsultationapp/openweathermap/ForecastOpenWeather.kt
+++ app/src/main/java/com/example/weatherconsultationapp/openweathermap/ForecastOpenWeather.kt
@@ -2,4 +2,19 @@ package com.example.weatherconsultationapp.openweathermap
 
 //
 class ForecastOpenWeather {
+
+    //
+    private var coords: Coordinates? = null
+    private var weather: Weather? = null
+    private var temperature: Temperature? = null
+    private var feelingLikeTemperature: Temperature? = null
+    private var minTemperature: Temperature? = null
+    private var maxTemperature: Temperature? = null
+    private var pressure: Pressure? = null
+    private var humidity: Humidity? = null
+    private var wind: Wind? = null
+    private var sunRiseTime: Time? = null
+    private var sunSetTime: Time? = null
+    private var geoLocation: GeographicLocation? = null
+    private var uv: UV? = null
 }
