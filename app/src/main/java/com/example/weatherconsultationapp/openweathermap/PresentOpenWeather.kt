00980d8f708dc14dacb24065175e2504eec12a75
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-03-06 19:58:21 +0100

    50 - 50 - (2)

--- app/src/main/java/com/example/weatherconsultationapp/openweathermap/PresentOpenWeather.kt
+++ app/src/main/java/com/example/weatherconsultationapp/openweathermap/PresentOpenWeather.kt
@@ -1,7 +1,17 @@
 package com.example.weatherconsultationapp.openweathermap
 
 //
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.runBlocking
+
+//
 class PresentOpenWeather {
+
+    //
+    private var parameters: Parameters? = null
 
     //
     constructor(localization: String, countryCode: String, apiKey: String) {
