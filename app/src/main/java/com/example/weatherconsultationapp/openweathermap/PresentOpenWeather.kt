5c78445f6dc91414f06882ccb6f31b0025537c8e
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-03-06 19:54:27 +0100

    50 - 50

--- app/src/main/java/com/example/weatherconsultationapp/openweathermap/PresentOpenWeather.kt
+++ app/src/main/java/com/example/weatherconsultationapp/openweathermap/PresentOpenWeather.kt
@@ -2,4 +2,19 @@ package com.example.weatherconsultationapp.openweathermap
 
 //
 class PresentOpenWeather {
+
+    //
+    constructor(localization: String, countryCode: String, apiKey: String) {
+
+    }
+
+    //
+    constructor(localization: String, apiKey: String) {
+
+    }
+
+    //
+    constructor(latitude: Double, longitude: Double, apiKey: String) {
+
+    }
 }
